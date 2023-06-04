10de7c7576b29a53d75987394594613f201325ea
DAWIT GURARA <dawit2nd@gmail.com>
2023-06-04 13:37:54 +0300

    update code and test case added

--- service/routes.py
+++ service/routes.py
@@ -85,7 +85,16 @@ def create_accounts():
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+    def update_accounts(account_id):
+        """
+        Update an Account
+        This endpoint will update an Account based the account_id that is requested
+        """
+        app.logger.info("Request to read an Account with id: %s", account_id)
+        account = Account.find(account_id)
+        if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,3 +139,20 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    # UPDATE ACCOUNT TEST CASE
+    
+    def test_update_account(self):
+        """It should update a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_update_account_not_found(self):
+        """It should not update an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
